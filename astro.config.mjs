3052a14eea50876410fe40780a8a9d4b4ce970bb
gagaok <gael.quid@gmail.com>
2024-12-01 11:53:10 +0100

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vojtastruhar.github.io/",
   base: "/steam-review-template",
   integrations: [tailwind()],
+  output: "server",
+  adapter: netlify(),
 });
