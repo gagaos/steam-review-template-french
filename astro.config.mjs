e06649c2230afda933e7cfec310454b4dd6e359a
gagaok <gael.quid@gmail.com>
2024-12-01 12:18:48 +0100

    Update with my github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vojtastruhar.github.io/",
+  site: "https://gagos.github.io",
   base: "/steam-review-template",
   integrations: [tailwind()],
 });
