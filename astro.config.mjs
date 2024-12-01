7399651bcc6c8ce5b3bb09a0d88c46efd8606109
gagaok <gael.quid@gmail.com>
2024-12-01 12:00:48 +0100

    Finalement, pas netify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://vojtastruhar.github.io/",
   base: "/steam-review-template",
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify(),
 });
